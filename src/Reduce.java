7170324938d715f9887f349fca96c8908ca643a4
fizah <fizahumayoonkabeer@gmail.com>
2024-09-12 15:10:49 -0400

    new file

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int steps = 0;
+        int i = 1000;
+
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+            }
+            else {
+                i--;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
+}
